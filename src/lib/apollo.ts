53908f17e0859afa7183d6e11391f806943b3ae6
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-06-26 19:02:38 -0300

    Aula 4 - Inscrição via GraphQL

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o9azfi1f2c01z244q986t2/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
