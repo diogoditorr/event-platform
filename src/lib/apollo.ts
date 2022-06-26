ac268a61fa5b084685d944202a6f76282a63a81b
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-06-26 11:48:33 -0300

    Ignite Lab | Aula 2

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: '',
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o9azfi1f2c01z244q986t2/master',
     cache: new InMemoryCache()
 })
