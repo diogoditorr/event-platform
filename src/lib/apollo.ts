ef8ee5b29d9caee105c134cb30430cfb421ac582
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-07-09 12:04:39 -0300

    refactor: rename environment variable

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: import.meta.env.VITE_API_URL,
+    uri: import.meta.env.VITE_GRAPHQL_API_URL,
     headers: {
-        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+        'Authorization': `Bearer ${import.meta.env.VITE_GRAPHQL_API_ACCESS_TOKEN}`
     },
     cache: new InMemoryCache()
 })
