c389f1743039db307165e2faa77efc1fc2a0eb32
Diego Oliveira <35296262+diogoditorr@users.noreply.github.com>
2022-07-12 00:35:38 -0300

    feat: update protected route component

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,16 +9,20 @@ import Subscribe from "./pages/Subscribe";
 export default function Router() {
     const { isAuthenticated, isLoading } = useAuth();
 
-    if (isLoading) {
-        return <AppLoading />;
-    }
-
     return (
         <Routes>
             <Route path="/" element={<Subscribe />} />
-            <Route element={<ProtectedRoute
-                isAllowed={isAuthenticated}
-            />}>
+            <Route
+                element={
+                    <ProtectedRoute
+                        isAllowed={isAuthenticated}
+                        fallback={{
+                            element: <AppLoading />,
+                            deps: [isLoading],
+                        }}
+                    />
+                }
+            >
                 <Route path="/event" element={<Event />} />
                 <Route path="/event/lesson/:slug" element={<Event />} />
             </Route>
--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,14 +4,25 @@ import { Navigate, Outlet } from "react-router-dom";
 type Props = {
     isAllowed: boolean;
     redirectPath?: string;
+    fallback?: {
+        element: ReactNode;
+        deps: boolean[];
+    };
     children?: ReactNode;
 };
 
 export default function ProtectedRoute({
     isAllowed,
     redirectPath = "/",
+    fallback,
     children,
 }: Props) {
+    if (fallback) {
+        if (fallback.deps.some((dep) => dep)) {
+            return <>{fallback.element}</>;
+        }
+    }
+
     if (!isAllowed) {
         return <Navigate to={redirectPath} replace />;
     }
